912b2d866c9a6bef20ddbc383655567006a0083e
pwaykole <prat.pattu@gmail.com>
2016-07-22 10:45:53 -0400

    md-table implementation

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -1,14 +1,24 @@
 angular.module('app.routes', ['ui.router'])
 .config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/admin');
+    $urlRouterProvider.otherwise('/');
 
     $stateProvider
 
-        .state('login', {
+        .state('home', {
+            url: '/',
+            templateUrl: 'app/views/pages/home.html',
+            controller: 'mainController as login'
+        })
+        .state('admin', {
             url: '/admin',
             templateUrl: 'app/views/pages/login.html',
             controller: 'mainController as login'
+        })
+        .state('Users', {
+            url: '/Users',
+            templateUrl: 'app/views/pages/users/all.html',
+            controller: 'userController'
         });
 
 });
