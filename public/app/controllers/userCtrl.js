ca61e710813d52c8adff9517f41cd9683cb23d59
pwaykole <prat.pattu@gmail.com>
2016-07-29 23:47:04 -0400

    Home page of dealrship with car model in backend

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -97,21 +97,22 @@ angular.module('userCtrl', ['ngMaterial', 'md.data.table', 'userService']).confi
         $scope.answer = function (answer) {
             $mdDialog.hide(answer);
         };
-    };
+
     // function to create a user
-    vm.saveUser = function () {
+    $scope.saveUser = function () {
         console.log("in the function");
-        vm.processing = true;
+        $scope.processing = true;
         // clear the message
         var message = '';
         var pinTo = "bottom";
         // use the create function in the userService
-        User.create(vm.userData).success(function (data) {
-            vm.processing = false;
+        User.create($scope.userData).success(function (data) {
+            $scope.processing = false;
             // clear the form
-            vm.userData = {};
+            $scope.userData = {};
             message = data.message;
             $mdToast.show($mdToast.simple().textContent(message).position(pinTo).hideDelay(3000));
         });
+    };
     };
 }]);
